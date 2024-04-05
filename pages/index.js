4ecd6cb458daa984e8a76cde5f8e093348cdbbbb
VONUHAU <vnhau0812@gmail.com>
2024-04-05 19:37:14 +0700

    git test new branch with disable merge button

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Test github 3 action on the update branch]</p>
+        <p>[Test github 5 action on the update branch]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
